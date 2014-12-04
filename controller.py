0cb958abd2dfd9a277ab76abc6d5d2b1115230fd
Rafael Silva Guimaraes <rafael@infomania.com.br>
2014-12-04 00:14:31 -0200

    Controller class
    Begin class

---
+++ controller.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""
+Rafael S. Guimaraes <rafaelg@ifes.edu.br>
+http://rafaelguimaraes.net
+"""
+
+
+class Controller(object):
+    """
+     Classe controller para iniciar o
+     serviço de integraçao com servidor BGP.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    pass
+
+
+
+
