682823bce10667d42caa1efe3d1311253415dbd4
Rafael Silva Guimaraes <rafael@infomania.com.br>
2014-12-15 11:14:25 -0200

    Controller
    - Change version ExaBGP: 3.4.4
    - Log on ExaBGP.

--- controller.py
+++ controller.py
@@ -16,28 +16,31 @@ class Controller(object):
     """
     def __init__(self):
         """
-            Inicializator
-            Queues for sender and receiver
+        Inicializator
+        Queues for sender and receiver
+        :rtype : object
         """
         self.sender_queue = Queue()
         self.receiver_queue = Queue()
+        self.message_sent = []
+        self.message_received = []
 
     def start(self):
         """
             Run threads for incoming and outgoing messages;
         """
-        self.sender = Thread(target=_sender, args=(self.message,self.sender_queue))
+        self.sender = Thread(target=_sender, args=(self.message_sent, self.sender_queue))
         self.sender.start()
 
-        self.receiver = Thread(target=_receiver, args=(self.message,self.receiver_queue))
+        self.receiver = Thread(target=_receiver, args=(self.message_received, self.receiver_queue))
         self.receiver.start()
 
 
 def _sender(message, queue):
     """ sender to router """
-    while True:
+    while len(message) > 0:
         try:
-            # Write commando to stdout
+            # Write commando to STDOUT
             line = sys.stdout
             line.write("%s\n" % message )
             line.flush()
@@ -61,10 +64,22 @@ def _receiver(message, queue):
                 raise KeyboardInterrupt('Interrupt received in select')
         except:
             pass
+def timed (message="", **preample):
+    import time
+    now = time.strftime('%a, %d %b %Y %H:%M:%S',time.localtime())
+    return "%s | %-8s | %-6d | %-13s | %s" % (now,'FORKED',os.getpid(), preample['name'], message)
 
 if __name__ == "__main__":
+    import datetime
+    import exabgp.version
+    preample = { "name": "SDNRouter",
+                 "date": datetime.datetime.now(),
+                 "version": "0.1",
+                 "exabgp_version": exabgp.version.version
+    }
+    sys.stderr.write(timed("-"*60+"\n", **preample))
+    sys.stderr.write(timed("Starting Controller...\n", **preample))
+    sys.stderr.write(timed("Starting integration with ExaBGP version %(exabgp_version)s\n" % (preample), **preample))
+    sys.stderr.write(timed("-"*60+"\n", **preample))
     controller = Controller()
-
-
-
-
+    controller.start()
